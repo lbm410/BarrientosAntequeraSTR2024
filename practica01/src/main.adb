a0f2e08288de430d23496b2c2b8410b9a47e858b
Lucas Barrientos <lbm410@inlumine.ual.es>
2024-03-19 19:40:45 +0100

    iaiaiaiai

--- practica01/src/main.adb
+++ practica01/src/main.adb
@@ -34,5 +34,35 @@ begin
    -- Creamos el archivo de salida
    Create(output, Out_File, "output.txt");
 
+   Put_Line(output,("k" & ASCII.HT & "ST1" & ASCII.HT & "ST2" & ASCII.HT
+            & "ST3" & ASCII.HT & "ST4" & ASCII.HT & "SC1" & ASCII.HT & "SC2" & ASCII.HT & "SR1" & ASCII.HT & "SD1"));
+
+   -- Resultados
+   Put_Line("Resultados del Ejercicio 1:");
+
+   New_Line;
+
    Put_Line(output,("k" & ASCII.HT & ASCII.HT & "ST1" & ASCII.HT & ASCII.HT & "ST2" & ASCII.HT & ASCII.HT & "ST3" & ASCII.HT & ASCII.HT & "ST4" & ASCII.HT & ASCII.HT & "SC1" & ASCII.HT & ASCII.HT & "SC2" & ASCII.HT & ASCII.HT & "SR1" & ASCII.HT & ASCII.HT & "SD1"));
+
+   -- Leemos el archivo y procesamos la salida
+   while not End_Of_File(input) loop
+
+      -- Guardamos valores
+      Get(input, k);
+      Get(input, SR1(i));
+      Get(input, ST4(i));
+      Get(input, ST3(i));
+
+      -- Ponemos en marcha las fórmulas
+      Tt(i) := Tt_f(ST1(i-1), ST2(i-1));
+      ST2(i) := ST2_f(SR1(i), ST4(i), ST1(i-1), SC1(0), Tt(i));
+      ST1(i) := ST1_f(ST2(i));
+      SD1(i) := SD1_f(SC2(0), ST2(i-1), ST3(i));
+
+      -- Seguridad en ST2
+      if (St2(i) > 93.0) then
+         Put_Line("Cuidado!");
+      end if;
+
+      -- Líneas añadidas al fichero de texto
 end Main;
